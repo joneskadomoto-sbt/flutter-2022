a5b3b41440ce5fec30329b0f5d860207a6cc518b
Jones Seiji Kadomoto Bezerra <jseiji@sbt.com.br>
2022-10-19 10:18:02 -0300

    criando stateful widget

--- flutter_2022/lib/pages/home_page.dart
+++ flutter_2022/lib/pages/home_page.dart
@@ -1,7 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_2022/controllers/home_controller.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SBT Lab'),
+      ),
+      body: Center(
+        child: Text(
+          'Contador: $counter',
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => setState(() {
+          counter++;
+        }),
+      ),
+    );
+  }
+}
+
+/* class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
@@ -25,3 +59,5 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+ */
+
