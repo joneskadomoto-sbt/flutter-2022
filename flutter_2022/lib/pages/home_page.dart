c1ac8f769ad8e7ac8401cbf77088635b9e7c20d0
Jones Seiji Kadomoto Bezerra <jseiji@sbt.com.br>
2022-10-19 11:06:35 -0300

    trabalhando com list view

--- flutter_2022/lib/pages/home_page.dart
+++ flutter_2022/lib/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_2022/controllers/home_controller.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,49 +14,25 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('SBT Lab'),
-      ),
-      body: Center(
-        child: Text(
-          'Contador: $counter',
-          textDirection: TextDirection.ltr,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () => setState(() {
-          counter++;
-        }),
-      ),
-    );
-  }
-}
-
-/* class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    var controller = HomeController.of(context);
+    final names = ['Diego', 'Jones', 'Alex', 'Rodrigo', 'Robert'];
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('SBT Lab'),
       ),
-      body: Center(
-        child: Text(
-          'Contador: ${controller.value}',
-          textDirection: TextDirection.ltr,
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: ListView.builder(
+          itemCount: names.length,
+          itemBuilder: (context, index) {
+            final name = names[index];
+            return ListTile(
+              title: Text(name),
+            );
+          },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () => controller.increment(),
       ),
     );
   }
 }
- */
-
